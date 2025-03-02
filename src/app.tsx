6c00b1c08347856b30eb03f664786564af1fb487
unknown <bgalvaomiranda302@gmail.com>
2025-03-02 20:11:07 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import logo from "./assets/logo.svg";
+
+export default function App() {
+  return (
+    <div>
+      <h1>teste1</h1>
+      <h2>teste2</h2>
+      <h3>teste3</h3>
+      <img src={logo} />
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./styles/vars.css";
+import "./styles/global.css";
+
+import App from "./app.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
